5b36750eade20bcd4b0eda7aca2694320bd70979
mathias <mathiasmeeus2@gmail.com>
2025-11-18 12:13:44 +0100

    Initial repo setup

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+group = "be.kdg.team22"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
