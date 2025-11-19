eb5576d0bdfc10397a33dfee104a0696c9e0b648
mathias <mathiasmeeus2@gmail.com>
2025-11-19 16:09:58 +0100

    chore:  - ProfileId now gets uuid from jwt  - deleted Application.yml

--- src/main/java/be/kdg/team22/userservice/profile/domain/profile/ProfileId.java
+++ src/main/java/be/kdg/team22/userservice/profile/domain/profile/ProfileId.java
@@ -1,5 +1,9 @@
 package be.kdg.team22.userservice.profile.domain.profile;
 
+import be.kdg.team22.userservice.profile.application.profile.MissingRequiredClaimException;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
+import org.springframework.security.oauth2.jwt.Jwt;
+
 import java.util.UUID;
 
 public record ProfileId(UUID value) {
@@ -7,7 +11,19 @@ public record ProfileId(UUID value) {
         if (value == null) throw new IllegalArgumentException("ProfileId cannot be null");
     }
 
+    public static ProfileId create(String value) {
+        return new ProfileId(UUID.fromString(value));
+    }
+
     public static ProfileId from(UUID uuid) {
         return new ProfileId(uuid);
+    }
+
+    public static ProfileId get(Jwt token) {
+        String sub = token.getClaimAsString(StandardClaimNames.SUB);
+        if (sub == null) {
+            throw new MissingRequiredClaimException("Missing claim: sub");
+        }
+        return create(sub);
     }
 }
