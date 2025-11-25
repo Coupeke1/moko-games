dca0547696286f46cedf42bb2562b88fb32b3963
mathias <mathiasmeeus2@gmail.com>
2025-11-25 11:19:29 +0100

    chore: changes to test

---
+++ src/test/java/GamesServiceApplicationTests.java
@@ -0,0 +1,14 @@
+import be.kdg.team22.gamesservice.config.TestcontainersConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfig.class)
+@SpringBootTest
+class GamesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
