205a0573411c0081b17928fa4319407a569cc82d
mathias <mathiasmeeus2@gmail.com>
2025-11-25 11:25:22 +0100

    chore: changes to test

--- src/test/java/be/kdg/team22/gamesservice/GamesServiceApplicationTests.java
+++ src/test/java/be/kdg/team22/gamesservice/GamesServiceApplicationTests.java
@@ -1,3 +1,5 @@
+package be.kdg.team22.gamesservice;
+
 import be.kdg.team22.gamesservice.config.TestcontainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
