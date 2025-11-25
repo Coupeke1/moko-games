82901e9eef7a897f530ca5233a6d32059a7d84d8
mathias <mathiasmeeus2@gmail.com>
2025-11-25 19:49:49 +0100

    chore:   - implemented findbyUserId

--- src/main/java/be/kdg/team22/userservice/domain/library/LibraryRepository.java
+++ src/main/java/be/kdg/team22/userservice/domain/library/LibraryRepository.java
@@ -1,8 +1,13 @@
 package be.kdg.team22.userservice.domain.library;
 
+import be.kdg.team22.userservice.infrastructure.library.jpa.LibraryEntryEntity;
 import org.jmolecules.ddd.annotation.Repository;
+
+import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface LibraryRepository {
 
+    List<LibraryEntryEntity> findByUserId(UUID userId);
 }
--- src/main/java/be/kdg/team22/userservice/infrastructure/library/DbLibraryRepository.java
+++ src/main/java/be/kdg/team22/userservice/infrastructure/library/DbLibraryRepository.java
@@ -1,10 +1,23 @@
 package be.kdg.team22.userservice.infrastructure.library;
 
 import be.kdg.team22.userservice.domain.library.LibraryRepository;
+import be.kdg.team22.userservice.infrastructure.library.jpa.JpaLibraryEntryRepository;
+import be.kdg.team22.userservice.infrastructure.library.jpa.LibraryEntryEntity;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class DbLibraryRepository implements LibraryRepository {
+    private final JpaLibraryEntryRepository repository;
 
+    public DbLibraryRepository(final JpaLibraryEntryRepository repository) {
+        this.repository = repository;
+    }
 
+    @Override
+    public List<LibraryEntryEntity> findByUserId(UUID userId) {
+        return repository.findByUserId(userId);
+    }
 }
