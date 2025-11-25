82d685801172b1ba0c4a685ebc2b8ca64086be30
niceduckdev <bottykaj@gmail.com>
2025-11-25 10:09:49 +0100

    added icon components

--- src/routes/library/components/card.tsx
+++ src/routes/library/components/card.tsx
@@ -1,3 +1,5 @@
+import ClockIcon from "@/components/icons/clock-icon";
+import UsersIcon from "@/components/icons/users-icon";
 import { Gap } from "@/components/layout/gap";
 import { Items } from "@/components/layout/items";
 import Row from "@/components/layout/row";
@@ -18,18 +20,12 @@ export default function LibraryCard({ title, image, playtime, friendCount }: Pro
                 <h3 className="font-bold text-lg">{title.substring(0, 15)}{title.length > 15 ? "..." : ""}</h3>
                 <Row gap={Gap.Large} responsive={false}>
                     <Row gap={Gap.None} items={Items.Center} responsive={false}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28" fill="currentColor" className="size-6">
-                            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clipRule="evenodd" />
-                        </svg>
-
+                        <ClockIcon />
                         <p>{playtime}</p>
                     </Row>
 
                     <Row gap={Gap.None} items={Items.Center} responsive={false}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28" fill="currentColor" className="size-6">
-                            <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
-                        </svg>
-
+                        <UsersIcon />
                         <p>{friendCount}</p>
                     </Row>
                 </Row>
--- src/routes/profile/profile.tsx
+++ src/routes/profile/profile.tsx
@@ -1,3 +1,4 @@
+import PlusIcon from "@/components/icons/plus-icon";
 import Column from "@/components/layout/column";
 import { Gap } from "@/components/layout/gap";
 import Grid from "@/components/layout/grid/grid";
@@ -38,9 +39,7 @@ export default function ProfilePage() {
                         />
 
                         <button className="bg-bg-2 hover:bg-bg-3 transition-colors duration-75 flex flex-col justify-center items-center cursor-pointer select-none x-4 py-2 rounded-lg h-48">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8 text-fg-2">
-                                <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clipRule="evenodd" />
-                            </svg>
+                            <PlusIcon />
                         </button>
                     </Grid>
                 </Section>
