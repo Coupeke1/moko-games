139230028891137dccd25e2f5707e1554ec36326
Lee Coppens <lee.coppens@hotmail.be>
2025-11-26 15:44:26 +0100

    Declare config as global var

---
+++ src/types/runtime-config.d.ts
@@ -0,0 +1,12 @@
+declare global {
+  interface Window {
+    __APP_CONFIG__?: {
+      authUrl?: string;
+      authRealm?: string;
+      authClientId?: string;
+      userService?: string;
+    };
+  }
+}
+
+export {};
