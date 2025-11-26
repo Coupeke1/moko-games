272620cd639570fc1d7c9efead8406b9aca22339
mathias <mathiasmeeus2@gmail.com>
2025-11-26 15:05:55 +0100

    chore:   - base for achievements

---
+++ src/main/java/be/kdg/team22/userservice/application/achievement/AchievementService.java
@@ -0,0 +1,7 @@
+package be.kdg.team22.userservice.application.achievement;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AchievementService {
+}
---
+++ src/main/java/be/kdg/team22/userservice/config/RabbitMQConfig.java
@@ -0,0 +1,28 @@
+package be.kdg.team22.userservice.config;
+
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Bean
+    SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory
+    ) {
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+
+        return factory;
+    }
+
+    @Bean
+    Jackson2JsonMessageConverter jacksonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
