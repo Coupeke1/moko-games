4183ad61c271534ab4ce5db150ac32a988251f04
Lee Coppens <lee.coppens@hotmail.be>
2025-11-27 15:04:19 +0100

    fix tic tac toe frontend

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,23 @@
 import GamePage from "@/routes/game/game.tsx"
-import {BrowserRouter, Route, Routes} from "react-router"
+import { BrowserRouter, Route, Routes } from "react-router"
 import Auth from "@/components/auth.tsx";
+import { useAuthStore } from "./stores/auth-store";
+import LoadingState from "./components/state/loading";
 
 function App() {
+    const initialized = useAuthStore(state => state.initialized);
+    const token = useAuthStore(state => state.token);
+
     return (
         <BrowserRouter>
             <Auth />
-
-            <Routes>
-                <Route path="/game/:id" element={<GamePage/>}/>
-            </Routes>
+            {!initialized || !token ? (
+                <LoadingState />
+            ) : (
+                <Routes>
+                    <Route path="/game/:id" element={<GamePage />} />
+                </Routes>
+            )}
         </BrowserRouter>
     )
 }
