d83247c16e41090ccbc9b2b6a02267c4fe43749d
niceduckdev <bottykaj@gmail.com>
2025-11-27 14:17:40 +0100

    removed unused model

--- src/main/java/be/kdg/team22/sessionservice/infrastructure/friends/FriendsOverviewResponse.java
+++
@@ -1,8 +0,0 @@
-package be.kdg.team22.sessionservice.infrastructure.friends;
-
-import java.util.List;
-
-public record FriendsOverviewResponse(
-        List<FriendsResponse> friends,
-        List<FriendsResponse> incoming,
-        List<FriendsResponse> outgoing) {}
