f4a3fb25408c6a25d75550926df91d0afc03722a
mathias <mathiasmeeus2@gmail.com>
2025-11-28 10:28:05 +0100

    chore:  - hierse

--- src/main/java/be/kdg/team22/tictactoeservice/api/ExceptionController.java
+++ src/main/java/be/kdg/team22/tictactoeservice/api/ExceptionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ExceptionController {
     @ExceptionHandler({
             BoardSizeException.class,
             CellOccupiedException.class,
