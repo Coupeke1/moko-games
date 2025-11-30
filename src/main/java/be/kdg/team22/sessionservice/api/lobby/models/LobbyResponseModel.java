3b54188ccda0e7bfaaec58d5804dd0d1b7876efe
niceduckdev <bottykaj@gmail.com>
2025-11-30 12:13:57 +0100

    sort players in model

--- src/main/java/be/kdg/team22/sessionservice/api/lobby/models/LobbyResponseModel.java
+++ src/main/java/be/kdg/team22/sessionservice/api/lobby/models/LobbyResponseModel.java
@@ -7,15 +7,15 @@ import be.kdg.team22.sessionservice.domain.lobby.settings.CheckersSettings;
 import be.kdg.team22.sessionservice.domain.lobby.settings.TicTacToeSettings;
 
 import java.time.Instant;
-import java.util.Set;
+import java.util.Comparator;
+import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 public record LobbyResponseModel(UUID id,
                                  UUID gameId,
                                  UUID ownerId,
-                                 Set<PlayerSummaryModel> players,
-                                 PlayerSummaryModel aiPlayer,
+                                 List<PlayerSummaryModel> players,
+                                 PlayerSummaryModel bot,
                                  int maxPlayers,
                                  LobbyStatus status,
                                  Instant createdAt,
@@ -30,10 +30,13 @@ public record LobbyResponseModel(UUID id,
                     new CheckersSettingsModel(c.boardSize(), c.flyingKings());
         };
 
+        List<PlayerSummaryModel> playerModels = lobby.players().stream().map(PlayerSummaryModel::from).sorted(Comparator.comparing(PlayerSummaryModel::username)).toList();
+
         PlayerSummaryModel botModel = null;
         if (lobby.hasBot())
             botModel = PlayerSummaryModel.from(lobby.bot());
 
-        return new LobbyResponseModel(lobby.id().value(), lobby.gameId().value(), lobby.ownerId().value(), lobby.players().stream().map(PlayerSummaryModel::from).collect(Collectors.toSet()), botModel, lobby.settings().maxPlayers(), lobby.status(), lobby.createdAt(), settingsModel, lobby.startedGameId().map(GameId::value).orElse(null));
+
+        return new LobbyResponseModel(lobby.id().value(), lobby.gameId().value(), lobby.ownerId().value(), playerModels, botModel, lobby.settings().maxPlayers(), lobby.status(), lobby.createdAt(), settingsModel, lobby.startedGameId().map(GameId::value).orElse(null));
     }
 }
