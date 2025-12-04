8485615b1104aa0b63d38eb0fa5c4e44242d3c21
Lee Coppens <lee.coppens@hotmail.be>
2025-12-04 17:18:12 +0100

    chore: implement basic project setup

---
+++ src/main/java/be/kdg/team22/gameaclservice/GameAclServiceApplication.java
@@ -0,0 +1,13 @@
+package be.kdg.team22.gameaclservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameAclServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameAclServiceApplication.class, args);
+    }
+
+}
