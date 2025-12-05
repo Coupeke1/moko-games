b3fdc0f16bf1f7b06fd0aaf9a20497d58e0903a6
niceduckdev <bottykaj@gmail.com>
2025-12-05 09:55:00 +0100

    fixed filterChains method

--- src/main/java/be/kdg/team22/sessionservice/config/SecurityConfig.java
+++ src/main/java/be/kdg/team22/sessionservice/config/SecurityConfig.java
@@ -26,10 +26,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors(cors -> cors.configurationSource(corsConfigurationSource())).csrf(AbstractHttpConfigurer::disable).sessionManagement(mgmt -> mgmt.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).authorizeHttpRequests(auth -> auth.requestMatchers("/ws/**").permitAll().requestMatchers("/api/lobbies/**").authenticated().anyRequest().permitAll().requestMatchers("/actuator/health", "/actuator/health/**").permitAll().anyRequest().authenticated()).oauth2ResourceServer(rs -> rs.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource())).csrf(AbstractHttpConfigurer::disable).sessionManagement(mgmt -> mgmt.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).authorizeHttpRequests(auth -> auth.requestMatchers("/ws/**").permitAll().requestMatchers("/actuator/health", "/actuator/health/**").permitAll().requestMatchers("/api/lobbies/**").authenticated().anyRequest().permitAll()).oauth2ResourceServer(rs -> rs.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
 
         return http.build();
     }
+
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
