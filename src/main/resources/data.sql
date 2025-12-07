8a4464d48d2121890308ab47667c2600d9702441
niceduckdev <bottykaj@gmail.com>
2025-12-07 19:04:19 +0100

    added default values in catalog

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO store_catalog (id, price, category, purchase_count)
+VALUES ('00000000-0000-0000-0000-000000000001',
+        25,
+        'PARTY',
+        21),
+       ('00000000-0000-0000-0000-000000000002',
+        45,
+        'PARTY',
+        54);
