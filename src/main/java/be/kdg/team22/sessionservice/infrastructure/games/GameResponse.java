ebdc99656def5d6a5c6564ddbed89c93ca291a42
mathias <mathiasmeeus2@gmail.com>
2025-12-08 17:15:07 +0100

    feat: extend GameResponse to include additional metadata fields

--- src/main/java/be/kdg/team22/sessionservice/infrastructure/games/GameResponse.java
+++ src/main/java/be/kdg/team22/sessionservice/infrastructure/games/GameResponse.java
@@ -1,7 +1,18 @@
 package be.kdg.team22.sessionservice.infrastructure.games;
 
+import java.time.Instant;
 import java.util.UUID;
 
-record GameResponse(UUID id, String name) {
+public record GameResponse(
+        UUID id,
+        String name,
+        String title,
+        String description,
+        String image,
+        String frontendUrl,
+        String startEndpoint,
+        Instant createdAt,
+        Instant updatedAt
+) {
 }
 
