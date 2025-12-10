3342793822a0797c118eb648060a16f80953cc04
niceduckdev <bottykaj@gmail.com>
2025-12-10 15:03:28 +0100

    added auth token

--- src/main/java/be/kdg/team22/sessionservice/infrastructure/chat/ExternalChatRepository.java
+++ src/main/java/be/kdg/team22/sessionservice/infrastructure/chat/ExternalChatRepository.java
@@ -4,6 +4,7 @@ import be.kdg.team22.sessionservice.domain.lobby.LobbyId;
 import be.kdg.team22.sessionservice.domain.lobby.exceptions.CommunicationServiceException;
 import be.kdg.team22.sessionservice.domain.lobby.exceptions.NotReachableException;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
@@ -13,16 +14,13 @@ import org.springframework.web.client.RestClientException;
 public class ExternalChatRepository {
     private final RestClient client;
 
-    public ExternalChatRepository(@Qualifier("communicationService") RestClient client) {
+    public ExternalChatRepository(@Qualifier("communicationService") final RestClient client) {
         this.client = client;
     }
 
-    public void createLobbyChat(LobbyId lobbyId) {
+    public void createLobbyChat(final LobbyId id, final Jwt token) {
         try {
-            client.post()
-                    .uri("/api/chat/channel/lobby/{id}", lobbyId.value().toString())
-                    .retrieve()
-                    .toBodilessEntity();
+            client.post().uri("/api/chat/channel/lobby/{id}", id.value().toString()).header("Authorization", String.format("Bearer %s", token.getTokenValue())).retrieve().toBodilessEntity();
         } catch (HttpClientErrorException e) {
             throw new CommunicationServiceException("CommunicationService refused chat creation: " + e.getStatusCode());
         } catch (RestClientException e) {
