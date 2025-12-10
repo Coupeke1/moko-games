23da68ddb0f0953a21cddd4a94c1c904c2f077e4
Lee Coppens <lee.coppens@hotmail.be>
2025-12-10 17:25:01 +0100

    chore: fix auth

--- src/app.tsx
+++ src/app.tsx
@@ -2,18 +2,28 @@ import GamePage from "@/routes/game/game"
 import {BrowserRouter, Route, Routes} from "react-router"
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 import Auth from "@/components/auth.tsx"
+import LoadingState from "@/components/state/loading.tsx";
+import {useAuthStore} from "@/stores/auth-store.ts";
 
 const queryClient = new QueryClient();
 
 function App() {
+    const initialized = useAuthStore((state) => state.initialized);
+    const token = useAuthStore((state) => state.token);
+
     return (
         <QueryClientProvider client={queryClient}>
-            <Auth />
             <BrowserRouter basename="/checkers">
-                <Routes>
-                    <Route path="/game/:gameId" element={<GamePage/>}/>
-                    <Route path="/" element={<GamePage/>}/>
-                </Routes>
+                <Auth />
+
+                {!initialized || !token ? (
+                    <LoadingState />
+                ) : (
+                    <Routes>
+                        <Route path="/game/:gameId" element={<GamePage/>}/>
+                        <Route path="/" element={<GamePage/>}/>
+                    </Routes>
+                )}
             </BrowserRouter>
         </QueryClientProvider>
     )
--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -1,24 +1,37 @@
-import {useAuthStore} from "@/stores/auth-store.tsx";
-import {useEffect} from "react";
-import LoadingState from "@/components/state/loading.tsx";
-import {environment} from "@/config.ts";
-
-const config: Keycloak.KeycloakConfig = {
-    url: environment.authUrl,
-    realm: environment.authRealm,
-    clientId: environment.authClientId
-};
+import Page from "@/components/layout/page";
+import LoadingState from "@/components/state/loading";
+import { useProfile } from "@/hooks/use-profile.ts";
+import { useAuthStore } from "@/stores/auth-store";
+import { useEffect } from "react";
+import ErrorState from "@/components/state/error";
+import { environment } from "@/config";
 
 export default function Auth() {
     const initAuth = useAuthStore((state) => state.init);
     const initialized = useAuthStore((state) => state.initialized);
+    const { loading: isLoading, error: isError } = useProfile();
 
     useEffect(() => {
-        initAuth(config);
+        initAuth({
+            url: environment.authUrl,
+            realm: environment.authRealm,
+            clientId: environment.authClientId,
+        } as Keycloak.KeycloakConfig);
     }, []);
 
-    if (!initialized) return (
-        <LoadingState />
-    )
+    if (!initialized || isLoading)
+        return (
+            <Page>
+                <LoadingState />
+            </Page>
+        );
+
+    if (isError)
+        return (
+            <Page>
+                <ErrorState />
+            </Page>
+        );
+
     return <></>;
 }
