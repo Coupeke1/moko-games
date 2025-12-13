75399ffbd64fbab379a92c08ddb3a2cd97726d65
mathias <mathiasmeeus2@gmail.com>
2025-12-13 15:58:34 +0100

    Replace `BoltIcon` and `BoltSlashIcon` with `ActiveIcon` and `NotActiveIcon`; remove unused `BoltIcon` component.

--- src/features/library/components/library-card.tsx
+++ src/features/library/components/library-card.tsx
@@ -5,8 +5,8 @@ import {Items} from "@/components/layout/items";
 import Row from "@/components/layout/row";
 import {Height} from "@/components/layout/size";
 import type {Entry} from "@/features/library/models/entry.ts";
-import BoltIcon from "@/components/icons/bolt-icon.tsx";
-import BoltSlashIcon from "@/components/icons/boltSlash-icon.tsx";
+import ActiveIcon from "@/components/icons/active-icon.tsx";
+import NotActiveIcon from "@/components/icons/not-active-icon.tsx";
 
 interface Props {
     entry: Entry;
@@ -21,7 +21,7 @@ export default function LibraryCard({entry}: Props) {
             height={Height.ExtraLarge}
             information={
                 <Row gap={Gap.Small} items={Items.Center} responsive={false}>
-                    {entry.healthy ? <BoltIcon/> : <BoltSlashIcon/>}
+                    {entry.healthy ? <ActiveIcon/> : <NotActiveIcon/>}
                     {entry.healthy ? "Online" : "Offline"}
                 </Row>
             }
