18c3bd0fe7500c938c7d91e98c894a27d719c770
mathias <mathiasmeeus2@gmail.com>
2025-12-13 16:41:42 +0100

    test: update library tests to include 'favourite' flag in game models

--- src/main/java/be/kdg/team22/userservice/config/RabbitMQTopology.java
+++ src/main/java/be/kdg/team22/userservice/config/RabbitMQTopology.java
@@ -23,7 +23,7 @@ public class RabbitMQTopology {
     Binding gameplayBinding() {
         return BindingBuilder.bind(userGameplayQueue())
                 .to(new TopicExchange(EXCHANGE_GAMEPLAY))
-                .with("game.#.achievements");
+                .with("game.#.achievement");
     }
 
     @Bean
