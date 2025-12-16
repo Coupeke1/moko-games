2b0308ae61d7c22f3477da480844c65dd2aea3fe
Lee Coppens <lee.coppens@hotmail.be>
2025-12-16 17:13:38 +0100

    feat: simplify achievement codes and add game name to events

--- src/main/java/be/kdg/team22/checkersservice/domain/events/AchievementCode.java
+++ src/main/java/be/kdg/team22/checkersservice/domain/events/AchievementCode.java
@@ -1,10 +1,10 @@
 package be.kdg.team22.checkersservice.domain.events;
 
 public enum AchievementCode {
-    CHECKERS_DRAW,
-    CHECKERS_LOSS,
-    CHECKERS_WIN,
-    CHECKERS_PROMOTION,
-    CHECKERS_MULTICAPTURE,
-    CHECKERS_THREE_KINGS
+    DRAW,
+    LOSS,
+    WIN,
+    PROMOTION,
+    MULTICAPTURE,
+    THREE_KINGS
 }
