ca33dfd5b59c4d2067d0523d003a5555cb01d65e
HangingGoose <matti.lehtinen@student.kdg.be>
2025-12-18 16:16:05 +0100

    Fallback functional, removed repeated polling

--- src/main/java/be/kdg/team22/checkersservice/infrastructure/bot/ExternalBotRepository.java
+++ src/main/java/be/kdg/team22/checkersservice/infrastructure/bot/ExternalBotRepository.java
@@ -5,7 +5,6 @@ import be.kdg.team22.checkersservice.domain.game.exceptions.BotServiceNotReachab
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
@@ -17,47 +16,26 @@ public class ExternalBotRepository {
     private final Logger logger;
     private final RestClient primaryClient;
     private final RestClient fallbackClient;
-    private final int MAX_POLLING_ATTEMPTS;
-    private final long POLLING_DELAY_MS;
 
     public ExternalBotRepository(
             @Qualifier("botService") final RestClient primaryClient,
-            @Qualifier("botServiceFallback") final RestClient fallbackClient,
-            @Value("${business.ai.max_polling_attempts}") final int MAX_POLLING_ATTEMPTS,
-            @Value("${business.ai.polling_delay_ms}") final long POLLING_DELAY_MS
-
+            @Qualifier("botServiceFallback") final RestClient fallbackClient
     ) {
         logger = LoggerFactory.getLogger(ExternalBotRepository.class);
         this.primaryClient = primaryClient;
         this.fallbackClient = fallbackClient;
-        this.MAX_POLLING_ATTEMPTS = MAX_POLLING_ATTEMPTS;
-        this.POLLING_DELAY_MS = POLLING_DELAY_MS;
     }
 
     public BotMoveResponse requestMove(BotMoveRequest request) {
         BotMoveResponse response = null;
-
-        for (int attempt = 0; attempt < MAX_POLLING_ATTEMPTS; attempt++) {
+        try {
+            response = tryRequestWithClient(primaryClient, "Primary", request);
+        } catch (BotMoveRequestFailedException | BotServiceNotReachableException ex) {
+            logger.error(ex.getMessage());
             try {
-                response = tryRequestWithClient(primaryClient, "Primary", request);
-                return response;
-            } catch (BotMoveRequestFailedException | BotServiceNotReachableException ex) {
-                logger.error(ex.getMessage());
-                try {
-                    response = tryRequestWithClient(fallbackClient, "Fallback", request);
-                } catch (BotMoveRequestFailedException | BotServiceNotReachableException fallbackEx) {
-                    logger.error(fallbackEx.getMessage());
-                    try {
-                        Thread.sleep(POLLING_DELAY_MS);
-                    } catch (InterruptedException interruptedEx) {
-                        Thread.currentThread().interrupt();
-                        logger.warn("Polling was interrupted", interruptedEx);
-                    }
-                }
-
-                if (response != null) {
-                    break;
-                }
+                response = tryRequestWithClient(fallbackClient, "Fallback", request);
+            } catch (BotMoveRequestFailedException | BotServiceNotReachableException fallbackEx) {
+                logger.error(fallbackEx.getMessage());
             }
         }
         return response;
