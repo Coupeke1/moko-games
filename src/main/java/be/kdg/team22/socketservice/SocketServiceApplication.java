b747a525fbf64da18baa1a8e5fb4376b9c11e7ce
niceduckdev <bottykaj@gmail.com>
2025-12-18 16:45:14 +0100

    added socket annotation

--- src/main/java/be/kdg/team22/socketservice/SocketServiceApplication.java
+++ src/main/java/be/kdg/team22/socketservice/SocketServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableWebSocketMessageBroker
 public class SocketServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SocketServiceApplication.class, args);
     }
-
 }
