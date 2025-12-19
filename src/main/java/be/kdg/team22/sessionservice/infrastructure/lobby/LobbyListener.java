2efe4355dad64c05b64ec902b892a16fadb43bc2
niceduckdev <bottykaj@gmail.com>
2025-12-19 16:11:08 +0100

    removed unused logging

--- src/main/java/be/kdg/team22/sessionservice/infrastructure/lobby/LobbyListener.java
+++ src/main/java/be/kdg/team22/sessionservice/infrastructure/lobby/LobbyListener.java
@@ -5,25 +5,27 @@ import be.kdg.team22.sessionservice.application.lobby.LobbyService;
 import be.kdg.team22.sessionservice.config.RabbitMQTopology;
 import be.kdg.team22.sessionservice.domain.lobby.Lobby;
 import be.kdg.team22.sessionservice.domain.player.PlayerId;
+import java.util.Optional;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
 public class LobbyListener {
+
     private final RabbitTemplate template;
     private final LobbyService service;
 
-    public LobbyListener(final RabbitTemplate template, final LobbyService service) {
+    public LobbyListener(
+        final RabbitTemplate template,
+        final LobbyService service
+    ) {
         this.template = template;
         this.service = service;
     }
 
     @RabbitListener(queues = RabbitMQTopology.QUEUE_SUBSCRIBED_SOCKET)
     public void subscribeListener(final SubscribeMessage message) {
-        System.out.println("    RECEIVED SUBSCRIBE");
         PlayerId playerId = PlayerId.from(message.userId());
 
         Optional<Lobby> lobby = service.findByPlayer(playerId);
@@ -35,7 +37,10 @@ public class LobbyListener {
     public void publishToPlayer(final PlayerId id, final Lobby lobby) {
         LobbyModel model = LobbyModel.from(lobby);
         LobbyMessage message = new LobbyMessage(id.value(), "lobby", model);
-        System.out.println("    SENDING LOBBY TO SUBSCRIBED PLAYER");
-        template.convertAndSend(RabbitMQTopology.EXCHANGE_USER_SOCKET, "user.message", message);
+        template.convertAndSend(
+            RabbitMQTopology.EXCHANGE_USER_SOCKET,
+            "user.message",
+            message
+        );
     }
 }
