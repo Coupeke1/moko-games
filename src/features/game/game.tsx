10fc49658691203c9f4e7114748792ba0a972b76
niceduckdev <bottykaj@gmail.com>
2025-12-20 16:13:21 +0100

    added correct height for iframe and chat

--- src/features/game/game.tsx
+++ src/features/game/game.tsx
@@ -6,26 +6,32 @@ import { useRef } from "react";
 
 export default function GamePage() {
     const { lobby, profile, game, loading, error } = useSession();
-    const frame = useRef(null);
+    const frame = useRef<HTMLIFrameElement | null>(null);
 
     return (
         <Page>
             <State
                 loading={loading}
                 error={error}
-                empty={!profile && !game && !profile}
+                empty={!profile && !game}
                 message="Game not found"
             >
                 {profile && lobby && game && (
-                    <section className={`grid sm:grid-cols-12 ${Gap.Medium}`}>
+                    <section
+                        className={`
+                            grid sm:grid-cols-12
+                            ${Gap.Medium}
+                            h-[calc(100vh-4rem)]
+                        `}
+                    >
                         <iframe
                             ref={frame}
                             src={`${game.frontendUrl}/${lobby.startedGameId}`}
-                            className="col-span-8 w-full rounded-lg h-88 bg-bg-3 border-3 border-bg-2"
                             title={game.title}
-                        ></iframe>
+                            className="col-span-8 w-full h-11/12 max-h-full rounded-lg bg-bg-3 border-3 border-bg-2"
+                        />
 
-                        <section className="col-span-4 bg-bg-3 rounded-lg flex items-center justify-center">
+                        <section className="col-span-4 h-11/12 max-h-full bg-bg-3 rounded-lg flex items-center justify-center">
                             <p>Chat</p>
                         </section>
                     </section>
