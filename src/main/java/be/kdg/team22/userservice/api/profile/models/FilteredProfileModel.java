aeebb80a55b6a8fed374d79583ec74f0a56b1694
niceduckdev <bottykaj@gmail.com>
2025-12-22 15:36:06 +0100

    changed model structure

--- src/main/java/be/kdg/team22/userservice/api/profile/models/FilteredProfileModel.java
+++ src/main/java/be/kdg/team22/userservice/api/profile/models/FilteredProfileModel.java
@@ -5,14 +5,10 @@ import be.kdg.team22.userservice.api.achievement.models.AchievementModel;
 import java.util.List;
 import java.util.UUID;
 
-public record FilteredProfileModel(
-        UUID id,
-        String username,
-        String description,
-        String image,
-        long level,
-        long playTime,
-        List<AchievementModel> achievements,
-        List<FavouriteGameModel> favouriteGames
-) {
-}
+public record FilteredProfileModel(UUID id,
+                                   String username,
+                                   String description,
+                                   String image,
+                                   StatisticsModel statistics,
+                                   List<AchievementModel> achievements,
+                                   List<FavouriteGameModel> favourites) {}
