c6d8e65bf289a3236991cbccf30c3299c5ce195a
niceduckdev <bottykaj@gmail.com>
2025-12-27 14:38:20 +0100

    mobile responsiveness + send bar

--- src/features/chat/chat.tsx
+++ src/features/chat/chat.tsx
@@ -1,20 +1,29 @@
 import { Gap } from "@/components/layout/gap";
 import Page from "@/components/layout/page";
 import BotCard from "@/features/chat/components/channel-card/bot-card";
+import SendBar from "@/features/chat/components/send-bar";
 /* import Message from "@/features/chat/components/message"; */
 
 export default function ChatPage() {
     return (
         <Page>
-            <section className={`grid sm:grid-cols-12 ${Gap.Medium}`}>
-                <section className="col-span-4 rounded-lg flex flex-col gap-2 h-[calc(100vh-12rem)] overflow-y-scroll">
+            <section className={`grid md:grid-cols-12 ${Gap.Medium}`}>
+                <section className="col-span-12 md:col-span-4 h-full rounded-lg flex flex-col gap-2 md:h-[calc(100vh-12rem)] overflow-y-scroll">
                     <BotCard selected={true} />
                 </section>
 
-                <section className="col-span-8 p-4 rounded-lg bg-bg-3 ">
-                    <section className="flex flex-col gap-8 h-[calc(100vh-13rem)] overflow-y-scroll">
+                <section
+                    className={`flex flex-col ${Gap.Medium} col-span-12 md:col-span-8`}
+                >
+                    <section className="p-4 rounded-lg bg-bg-3 flex flex-col gap-8 h-32 md:h-[calc(100vh-13rem)] overflow-y-scroll">
                         {/* <Message sender={null} message={null} /> */}
                     </section>
+
+                    <SendBar
+                        onSend={(message: string) => {
+                            console.log(message);
+                        }}
+                    />
                 </section>
             </section>
         </Page>
---
+++ src/features/chat/components/send-bar.tsx
@@ -0,0 +1,37 @@
+import Button from "@/components/buttons/button";
+import Form from "@/components/inputs/form";
+import Input from "@/components/inputs/input";
+import { Gap } from "@/components/layout/gap";
+import { useState, type FormEvent } from "react";
+
+interface Props {
+    onSend: (message: string) => void;
+}
+
+export default function SendBar({ onSend }: Props) {
+    const [message, setMessage] = useState("");
+
+    const handleSearch = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (message.length <= 0) return;
+
+        onSend(message);
+        setMessage("");
+    };
+
+    return (
+        <Form submit={handleSearch}>
+            <section className={`grid sm:grid-cols-4 ${Gap.Medium}`}>
+                <section className="sm:col-span-3">
+                    <Input
+                        placeholder="How are you doing?"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    />
+                </section>
+
+                <Button fullWidth={true}>Send</Button>
+            </section>
+        </Form>
+    );
+}
