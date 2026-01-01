54e1fe08f248a284c6b2757a51d186396e22c73c
mathias <mathiasmeeus2@gmail.com>
2026-01-01 18:59:47 +0100

    Update `watchLobby` to improve type safety and simplify logic
    - Replaced `Lobby` with `LobbyMessage` in return type for better alignment with message parsing.
    - Simplified return statement by combining `client.watch` and `map`.
    - Adjusted import formatting for consistency.

--- src/features/lobby/services/socket.ts
+++ src/features/lobby/services/socket.ts
@@ -1,14 +1,12 @@
-import type { Lobby } from "@/features/lobby/models/lobby.ts";
-import { useSocketStore } from "@/stores/socket-store.ts";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import type {LobbyMessage} from "@/features/lobby/models/lobby.ts";
+import {useSocketStore} from "@/stores/socket-store.ts";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
 
-export function watchLobby(): Observable<Lobby> {
+export function watchLobby(): Observable<LobbyMessage> {
     const client = useSocketStore.getState().client;
 
-    const updates = client
-        .watch({ destination: "/user/queue/lobby" })
-        .pipe(map((msg) => JSON.parse(msg.body)));
-
-    return updates;
+    return client
+        .watch({destination: "/user/queue/lobby"})
+        .pipe(map((msg) => JSON.parse(msg.body) as LobbyMessage));
 }
