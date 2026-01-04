9d58c29c5783867506be27083f46552036afd0dd
niceduckdev <bottykaj@gmail.com>
2026-01-04 19:11:16 +0100

    only show new notifications

--- src/components/global/notifications.tsx
+++ src/components/global/notifications.tsx
@@ -1,14 +1,23 @@
 import { showNotification } from "@/components/global/toast/toast";
 import { useNotification } from "@/features/notifications/hooks/use-notification";
 import type { Notification } from "@/features/notifications/models/notification";
+import { useEffect, useRef } from "react";
 
 export default function Notifications() {
     const { notifications, loading, error } = useNotification();
+    const lastIndex = useRef(0);
 
-    if (!notifications || loading || error) return null;
-    if (notifications.length <= 0) return null;
+    useEffect(() => {
+        if (!notifications || loading || error) return;
 
-    notifications.forEach((notification: Notification) => {
-        showNotification(notification.title, notification.message);
-    });
+        notifications
+            .slice(lastIndex.current)
+            .forEach((notification: Notification) => {
+                showNotification(notification.title, notification.message);
+            });
+
+        lastIndex.current = notifications.length;
+    }, [notifications, loading, error]);
+
+    return null;
 }
