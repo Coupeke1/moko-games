39fd2480bce0b0437544df41988423aa5076fb77
mathias <mathiasmeeus2@gmail.com>
2026-01-22 16:12:18 +0100

    Removed single submodule

--- infra/local-development/local-dev-containers/init-scripts/sql/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE SCHEMA games_service;
-CREATE SCHEMA store_service;
-CREATE SCHEMA social_service;
-CREATE SCHEMA communication_service;
-CREATE SCHEMA session_service;
-CREATE SCHEMA user_service;
-CREATE SCHEMA chess;
--- infra/local-development/local-dev-containers/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-
-docker volume prune
-docker network prune
